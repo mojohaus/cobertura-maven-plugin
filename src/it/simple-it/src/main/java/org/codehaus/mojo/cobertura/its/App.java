deb3d2b9e49f38862d40fab7f2abad1b3b958605
Olivier Lamy <olamy@apache.org>
2009-09-11 21:03:29 +0000

    add a cobertura report it.
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/cobertura-maven-plugin@10671 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/it/simple-it/src/main/java/org/codehaus/mojo/cobertura/its/App.java
@@ -0,0 +1,13 @@
+package org.codehaus.mojo.cobertura.its;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
