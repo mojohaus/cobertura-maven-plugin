bbc7725a11d61b80e30ee121e5a4b7e8f54284f0
Olivier Lamy <olamy@apache.org>
2009-09-11 21:31:38 +0000

    add a cobertura check goal it.
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/cobertura-maven-plugin@10673 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/it/check-it/src/main/java/org/codehaus/mojo/cobertura/its/App.java
@@ -0,0 +1,27 @@
+package org.codehaus.mojo.cobertura.its;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public void foo()
+    {
+        // in french toto means foo :-) 
+        WonderfullGreatObject toto = new WonderfullGreatObject();
+    }
+    
+    private static class WonderfullGreatObject()
+    {
+        private int age;
+        
+        
+        
+    }
+}
