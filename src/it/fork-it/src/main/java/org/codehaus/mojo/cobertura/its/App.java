caa5fcc83c978cc98c5ec77e76d8a9e6a5e843ee
Benson Margulies <bimargulies@gmail.com>
2011-04-23 23:57:17 +0000

    Make a copy of check-it to test side-effects on surefire fork mode.
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/cobertura-maven-plugin@13983 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/it/fork-it/src/main/java/org/codehaus/mojo/cobertura/its/App.java
@@ -0,0 +1,16 @@
+package org.codehaus.mojo.cobertura.its;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    
+    public boolean foo()
+    {
+        System.out.println( " hello toto, in english hello foo" );
+        return true;
+    }
+
+}
